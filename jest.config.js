fa9396b75d4ff199ae743143fa1cc08cdb24bd4b
Ryan Sonshine <sonshine@amazon.com>
2021-06-23 20:04:50 -0400

    fix(paths): update test paths to keep cli in root of lib

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/test/**/*.spec.ts'],
+  testMatch: ['**/__tests__/**/*.spec.ts'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/__tests__/**/*.ts',
     '!<rootDir>/src/types/**/*.ts',
     '!<rootDir>/src/cli.ts',
   ],
--- src/__tests__/integration/bin.js
+++ src/__tests__/integration/bin.js
@@ -2,4 +2,4 @@
 'use strict';
 
 require('ts-node/register');
-require('../../src/cli');
+require('../../cli');
