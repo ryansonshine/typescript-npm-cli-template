5dbc035874cb9df1ddebf048a72f9aec816418e6
Ryan Sonshine <sonshine@amazon.com>
2022-11-18 13:15:39 +0700

    test(helpers): remove test helper and update test

--- src/__tests__/integration/main.spec.ts
+++ src/__tests__/integration/main.spec.ts
@@ -1,8 +1,11 @@
-import { runCLI } from '../helpers';
+import execa from 'execa';
+import { resolve } from 'path';
+
+const bin = resolve(__dirname, './bin.js');
 
 describe('my-command', () => {
-  it('should display the help contents', () => {
-    const { stdout } = runCLI(process.cwd(), ['--help']);
+  it('should display the help contents', async () => {
+    const { stdout } = await execa(bin, ['--help']);
 
     expect(stdout).toContain('Usage: my-command [options]');
   });
