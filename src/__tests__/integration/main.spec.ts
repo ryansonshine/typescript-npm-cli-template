83119922fd47d05a2eb2174ea7bc5a483b723fdc
Niall O'Higgins <nialljohiggins@gmail.com>
2023-09-27 17:07:40 -0700

    Pass TS_NODE_FILES=true to ts-node in integration tests.
    This is needed to make sure that integration tests can pick up custom
    type declaration files which are sometimes needed for thirdparty JS
    modules.

--- src/__tests__/integration/main.spec.ts
+++ src/__tests__/integration/main.spec.ts
@@ -5,7 +5,9 @@ const bin = resolve(__dirname, './bin.js');
 
 describe('my-command', () => {
   it('should display the help contents', async () => {
-    const { stdout } = await execa(bin, ['--help']);
+    const { stdout } = await execa(bin, ['--help'], {
+      env: { TS_NODE_FILES: 'true' },
+    });
 
     expect(stdout).toContain('Usage: my-command [options]');
   });
